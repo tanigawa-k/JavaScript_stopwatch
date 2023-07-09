3b50fdb35f8957ea3b49e14a7d4c21b6ea40fbed
EC2 Default User <ec2-user@ip-172-31-23-138.ap-northeast-1.compute.internal>
2023-07-09 22:12:46 +0000

    ＜実践課題＞JavaScriptストップウォッチ-firstcommit

---
+++ main.js
@@ -0,0 +1,42 @@
+const time = document.getElementById('time');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+let startTime;
+let stopTime = 0;
+let timeoutID;
+
+function displayTime() {
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const h = String(currentTime.getHours()-9);
+  const m = String(currentTime.getMinutes());
+  const s = String(currentTime.getSeconds());
+  const ms = String(currentTime.getMilliseconds()).slice(0,1);
+  time.textContent = `${h}:${m}:${s}:${ms}`;
+  timeoutID = setTimeout(displayTime, 100);
+}
+
+startButton.addEventListener('click', function() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = true;
+  startTime = Date.now();
+  displayTime();
+});
+
+stopButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+  clearTimeout(timeoutID);
+  stopTime += (Date.now() - startTime);
+});
+
+resetButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+  time.textContent = '0:0:0:0';
+  stopTime = 0;
+});
